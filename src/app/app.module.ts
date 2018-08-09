0704ac1a9085d14094910c2403f80d2e18af1b3f
Liga Daine <liga.daine@daine-it.com>
2018-08-09 14:38:56 +0200

    Tabs Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { TabsPage } from './../pages/tabs/tabs';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -5,12 +6,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp, 
+    TabsPage
   ],
   imports: [
     BrowserModule,
@@ -18,8 +18,8 @@ import { HomePage } from '../pages/home/home';
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp, 
+    TabsPage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'page-tabs', 
+    templateUrl: 'tabs.html'
+})
+export class TabsPage {
+    favoritesPage = "FavoritesPage"; 
+    libraryPage = "LibraryPage";
+}
